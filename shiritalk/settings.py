229a246168041a68fca380a32e120e1ed7b81773
dn6150 <danool0821@gmail.com>
2023-08-12 17:39:57 +0900

    Update settings.py

--- shiritalk/settings.py
+++ shiritalk/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from pathlib import Path
 import re
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 STDICT_KEY = 'C200E92E3D4BF9A38ED1F7E5D5F823C0'
